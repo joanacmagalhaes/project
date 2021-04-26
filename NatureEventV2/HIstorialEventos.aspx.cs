3f52a45ae8d29efea46e7d72702e1ffb61d0b8bf
Joana <joanamagalhaes96@gmail.com>
2021-04-26 11:27:18 +0200

    historial eventos

--- NatureEventV2/HIstorialEventos.aspx.cs
+++ NatureEventV2/HIstorialEventos.aspx.cs
@@ -41,9 +41,10 @@ namespace NatureEventV2
                     eventos = dalEvento.SelectListEventosByIdUsuario((int)Session["idUser"]);
                     foreach (Evento evento in eventos)
                     {
-                        ContentArea.Controls.Add(new LiteralControl("<div style='border-style:solid; border-width:thin; border-radius: 25px; margin:auto; width:50%; background-color:white;' class='col-md-6 col-lg-6 position-static p-4 mb-3'><h5>" + evento.Nombre + "</h5><p>" + evento.Descripcion.ToString() + "</p>"));
+                        ContentArea.Controls.Add(new LiteralControl("<div style='margin:auto; width:50%; background-color:#D4EDE6; overflow: auto;' class='col-md-6 col-lg-6 position-static p-4 mb-3'>"));
+                        ContentArea.Controls.Add(new LiteralControl("<img src='https://picsum.photos/100' class='img-radius' style='float:left; margin-right:10%;'><h5>" + evento.Nombre + "</h5><p>" + evento.Descripcion.ToString()));
 
-                        ContentArea.Controls.Add(new LiteralControl("<a href='PaginaEvento.aspx?idEvento=" + evento.IdEvento + "' runat='server'>Click par la pagina principal</a></div>"));
+                        ContentArea.Controls.Add(new LiteralControl("</p><a href='PaginaEvento.aspx?idEvento=" + evento.IdEvento + "' runat='server'>Click par la pagina principal</a></div>"));
                     }
                 }
             }
